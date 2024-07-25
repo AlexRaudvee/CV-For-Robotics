7433f5e98b8ceb0644f033626cf4a40ea31578eb
20220698 <a.raudvee@student.tue.nl>
2024-07-25 02:11:02 +0200

    some play around + a way to work with video

--- YOLO+NAS.py
+++ YOLO+NAS.py
@@ -1,1 +1,13 @@
 # try out this way: https://medium.com/@Mert.A/how-to-use-yolov8-and-yolo-nas-for-object-detection-8c5893939480
+
+
+from super_gradients.training import models
+import cv2
+import torch
+
+device = 'cuda' if torch.cuda.is_available() else "cpu"
+
+model = models.get("yolo_nas_m", pretrained_weights="coco")
+
+img_path = "assets/semantic_segmentation/in.jpg"
+model.predict(img_path, conf=0.25).show()
